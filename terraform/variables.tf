ec9f60629e745d595d4713e0937126bfebf6766c
Mykyta Tarasov <mtarasov@gbsfo.com>
2024-10-02 14:00:11 +0300

    fix ami instanses codes

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -16,8 +16,7 @@ variable "ec2_instance_name" {
 variable "ami-ec2-images" {
   description = "Which AMI use."
   default = {
-    us-east-1 = "ami-05fa00d4c63e32376"
-    us-east-2 = "ami-0568773882d492fc8"
+    eu-central-1 = "ami-0084a47cc718c111a"
   }
 }
 ## Task 2: Basic Infrastructure Configuration
@@ -36,13 +35,4 @@ variable "private_subnet_cidrs" {
  type        = list(string)
  description = "Private Subnet CIDR values"
  default     = ["10.0.3.0/24", "10.0.4.0/24"]
-}
-
-variable "instance_type" {
-  default = "t2.micro"
-}
-
-variable "ec2_instance_name" {
-  description = "Name of the EC2 instance"
-  default     = "terraform-task2"
 }
