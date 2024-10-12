e751c084f0abd8e6e68abe86373ba20fcbb51139
Mykyta Tarasov <mtarasov@gbsfo.com>
2024-10-12 18:36:27 +0300

    set acl to one network

--- terraform/security.tf
+++ terraform/security.tf
@@ -42,7 +42,7 @@ resource "aws_security_group" "ec2" {
 
 resource "aws_network_acl" "network_80" {
   vpc_id     = aws_vpc.root.id
-  subnet_ids = aws_subnet.public_subnets[0].id
+  subnet_ids = [aws_subnet.public_subnets[0].id]
 
   egress {
     protocol  = "tcp"
@@ -69,7 +69,7 @@ resource "aws_network_acl" "network_80" {
 
 resource "aws_network_acl" "network_443" {
   vpc_id     = aws_vpc.root.id
-  subnet_ids = aws_subnet.public_subnets[0].id
+  subnet_ids = [aws_subnet.public_subnets[0].id]
   egress {
     protocol  = "tcp"
     rule_no   = 200
