051a46e58a37230b9d93ce4a59e1b5586ada7ca8
Mykyta Tarasov <mtarasov@gbsfo.com>
2024-10-12 18:05:07 +0300

    added http group to bastion

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "bastion" {
   instance_type               = var.instance_type
   iam_instance_profile        = aws_iam_instance_profile.session-manager.id
   associate_public_ip_address = true
-  security_groups             = [aws_security_group.ec2.id]
+  security_groups             = [aws_security_group.ec2.id, aws_security_group.http.id]
   
   subnet_id                   = aws_subnet.public_subnets[0].id
   tags = {
