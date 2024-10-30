54295187406c1391f0fe2e644d7f113f42d6c737
Mykyta Tarasov <mtarasov@gbsfo.com>
2024-10-30 16:33:11 +0200

    return echo, delete key_name

--- terraform/kube_instans.tf
+++ terraform/kube_instans.tf
@@ -4,15 +4,15 @@ resource "aws_instance" "kube_master" {
   iam_instance_profile        = aws_iam_instance_profile.session-manager.id
   associate_public_ip_address = false
   security_groups             = [aws_security_group.sg_https_ssh.id]
-  key_name = var.ssh_public_key
 
   subnet_id = aws_subnet.public_subnets[0].id
 
   user_data = <<-EOF
     #!/bin/bash
+    echo "${var.ssh_public_key}" >> /home/ubuntu/.ssh/authorized_keys
     apt-get update -y > /dev/null 2>&1
     apt-get install -y curl > /dev/null 2>&1
-    curl -sfL https://get.k3s.io | sh > /dev/null 2>&1
+    curl -sfL https://get.k3s.io
   EOF
 
   root_block_device {
